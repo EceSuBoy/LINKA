ea64a2795dff6ef684fbbb21023738118d78667d
Ece Su Boy <boyecesu@gmail.com>
2026-05-05 19:52:42 +0300

    Add project files.

---
+++ Services/Catalog/Linka.Catalog/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Linka.Catalog.Entities
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
