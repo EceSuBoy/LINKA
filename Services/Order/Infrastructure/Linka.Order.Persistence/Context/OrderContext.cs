ff73b991f50f7997cef8f1e805087e4014f0a7a9
Ece Su Boy <boyecesu@gmail.com>
2026-05-06 23:12:41 +0300

    Repository Class

---
+++ Services/Order/Infrastructure/Linka.Order.Persistence/Context/OrderContext.cs
@@ -0,0 +1,23 @@
+﻿using Linka.Order.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linka.Order.Persistence.Context
+{
+    public class OrderContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;initial Catalog=LinkaDiscountDb;integrated Security=true;TrustServerCertificate=True");
+        }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Ordering> Orderings { get; set; }
+
+
+    }
+}
