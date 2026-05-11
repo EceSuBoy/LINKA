a1e173981e23dbf6eaef780282f3421b5e6dcc93
Ece Su Boy <boyecesu@gmail.com>
2026-05-11 21:41:20 +0300

    Identity Microservice

---
+++ IdentityServer/Linka.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Identity;
+using Linka.IdentityServer.Dtos;
+using Linka.IdentityServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Linka.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            var values = new ApplicationUser()
+            {
+                UserName= userRegisterDto.Username,
+                Email= userRegisterDto.Email,
+                Name= userRegisterDto.Name,
+                Surname= userRegisterDto.Surname,
+            };
+            var result = await _userManager.CreateAsync(values, userRegisterDto.Password);
+            if(result.Succeeded)
+            {
+                return Ok("User added successfully");
+            }
+            else
+            {
+                return Ok("An error occured. Please try again.");
+            }
+        }
+    }
+}
--- IdentityServer/Linka.IdentityServer/Dtos/UserRegisterDto.cs
+++ IdentityServer/Linka.IdentityServer/Dtos/UserRegisterDto.cs
@@ -2,5 +2,11 @@
 {
     public class UserRegisterDto
     {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+
     }
 }
