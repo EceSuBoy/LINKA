089abdfef6c14aa8e9d3903fca8b5d89e5efe6dc
Ece Su Boy <boyecesu@gmail.com>
2026-05-12 15:07:16 +0300

    Generic Repositoyr and Entity Frameworks

---
+++ Services/Cargo/Linka.Cargo.DataAccessLayer/EntityFramework/EFCargoCompanyDal.cs
@@ -0,0 +1,20 @@
+﻿using Linka.Cargo.DataAccessLayer.Abstract;
+using Linka.Cargo.DataAccessLayer.Concrete;
+using Linka.Cargo.DataAccessLayer.Repositories;
+using Linka.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linka.Cargo.DataAccessLayer.EntityFramework
+{
+    public class EFCargoCompanyDal: GenericRepository<CargoCompany>, ICargoCompanyDal
+    {
+        public EFCargoCompanyDal(CargoContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ Services/Cargo/Linka.Cargo.DataAccessLayer/EntityFramework/EFCargoDetailDal.cs
@@ -0,0 +1,19 @@
+﻿using Linka.Cargo.DataAccessLayer.Abstract;
+using Linka.Cargo.DataAccessLayer.Concrete;
+using Linka.Cargo.DataAccessLayer.Repositories;
+using Linka.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linka.Cargo.DataAccessLayer.EntityFramework
+{
+    public class EFCargoDetailDal: GenericRepository<CargoDetail>, ICargoDetailDal
+    {
+        public EFCargoDetailDal(CargoContext context) : base(context)
+        {
+        }
+    }
+}
