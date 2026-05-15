b7b697a23ad7d2671ab8fcc4c2d91dfc34350f8f
Ece Su Boy <boyecesu@gmail.com>
2026-05-15 21:10:47 +0300

    Comment Microservice and Migration

---
+++ Services/Comment/Linka.Comment/Context/CommentContext.cs
@@ -0,0 +1,14 @@
+﻿using Linka.Comment.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Linka.Comment.Context
+{
+    public class CommentContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1442;initial Catalog=LinkaCommentDb;TrustServerCertificate=True;User=sa;Password=123456aA*");
+        }
+        public DbSet<UserComment> UserComments { get; set; }
+    }
+}
