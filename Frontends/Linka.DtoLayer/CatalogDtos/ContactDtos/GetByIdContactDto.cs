fdb9cc1f885fa61e169df00fa2dafbf21fe4aa2d
Ece Su Boy <boyecesu@gmail.com>
2026-05-16 13:36:15 +0300

    Contact Operations

---
+++ Frontends/Linka.DtoLayer/CatalogDtos/ContactDtos/GetByIdContactDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Linka.DtoLayer.CatalogDtos.ContactDtos
+
+{
+    public class GetByIdContactDto
+    {
+        public string ContactId { get; set; }
+        public string NameSurname { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public string IsRead { get; set; }
+        public DateTime SendDate { get; set; }
+    }
+}
--- Services/Catalog/Linka.Catalog/Controllers/ContactsController.cs
+++ Services/Catalog/Linka.Catalog/Controllers/ContactsController.cs
@@ -53,4 +53,4 @@ namespace Linka.Catalog.Controllers
         }
     }
 }
-}
+
