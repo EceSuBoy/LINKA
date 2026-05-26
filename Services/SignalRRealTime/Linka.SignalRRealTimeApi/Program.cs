79ec27bacfe66352afc3ce178a234a4a12a7a523
Ece Su Boy <boyecesu@gmail.com>
2026-05-26 19:01:28 +0300

    Connected Operations

--- Services/SignalRRealTime/Linka.SignalRRealTimeApi/Program.cs
+++ Services/SignalRRealTime/Linka.SignalRRealTimeApi/Program.cs
@@ -1,3 +1,5 @@
+using Linka.SignalRRealTimeApi.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,8 @@ builder.Services.AddCors(opt =>
                 .AllowCredentials();
     });
 });
+
+builder.Services.AddSignalR();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -33,5 +37,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<SignalRHub>("/signalrhub");
 
 app.Run();
