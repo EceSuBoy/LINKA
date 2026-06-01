d03cc188a3054c2811ea9e17da93ac4801b70cfb
Ece Su Boy <boyecesu@gmail.com>
2026-06-01 17:46:52 +0300

    RabbitMQ and RapidAPI

---
+++ RabbitMQMessage/Linka.RabbitMQMessageApi/Controllers/MessageController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Linka.RabbitMQMessageApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateMessage()
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost"      
+            };
+
+            var connection = connectionFactory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("Queue2", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            var messageContent = "The weather is hot today.";
+
+            var byteMessageContent = Encoding.UTF8.GetBytes(messageContent);
+
+            channel.BasicPublish(exchange: "", routingKey: "Queue2", basicProperties: null, body: byteMessageContent);
+
+            return Ok("Your message is in queue.");
+        }
+
+        private static string message;
+
+        [HttpGet]
+        public IActionResult ReadMessage()
+        {
+            var factory = new ConnectionFactory();
+
+            factory.HostName = "localhost";
+
+            var connection = factory.CreateConnection();    
+            var channel = connection.CreateModel();
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, x) =>
+            {
+                var byteMessage = x.Body.ToArray();
+                message = Encoding.UTF8.GetString(byteMessage);
+                
+            };
+            channel.BasicConsume(queue: "Queue1", autoAck: false, consumer:consumer);
+
+            if (string.IsNullOrEmpty(message))
+            {
+                return NoContent();
+
+            }
+            else
+            {
+                return Ok(message);
+            }
+                
+        }
+    }
+}
---
+++ RapidApi/Linka.RapidApiUI/Controllers/DefaultController.cs
@@ -0,0 +1,34 @@
+﻿using Linka.RapidApiUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Linka.RapidApiUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public async Task<IActionResult> WeatherDetail ()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://open-weather13.p.rapidapi.com/city?city=new%20york&lang=EN"),
+                Headers =
+    {
+        { "x-rapidapi-key", "0c7b05080fmsh8249ed924f337d9p10e459jsn49ac3df6fbdb" },
+        { "x-rapidapi-host", "open-weather13.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var weather = JsonConvert.DeserializeObject<WeatherViewModel.Rootobject>(body);
+
+                ViewBag.temp = weather.main.temp;
+                return View();
+            }
+        }
+    }
+}
